340f9ed4627b63271f24ccffc82385fd6fe8a7fd
benrau87 <benrau87@users.noreply.github.com>
2016-10-05 15:14:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 #####The following are just available tools that *should* be installed with Kali###
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
 
 mkdir ~/git
 
 cd ~/git
+
+echo "Downloading Discovery"
+git clone --quiet https://github.com/leebaird/discover.git
+cd discover
+./update.sh
+
+cd ~/git
+
+echo "Downloading Seclists"
+git clone --quiet https://github.com/benrau87/SecLists.git
+
+echo "Downloading PowerSploit"
+git clone --quiet https://github.com/PowerShellMafia/PowerSploit.git
+
+echo "Downloading EmPyre"
+git clone --quiet https://github.com/adaptivethreat/EmPyre.git
+
+echo "Downloading MailSniper"
+git clone --quiet https://github.com/dafthack/MailSniper.git
+
+echo "Downloading DomainPassSpray"
+git clone --quiet https://github.com/dafthack/DomainPasswordSpray.git
+
+echo "Downloading DomainPassSpray"
+git clone --quiet https://github.com/adaptivethreat/BloodHound.git
 
